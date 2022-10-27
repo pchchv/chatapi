415bb79894be11c130e59414323d0031b3cccba4
pchchv <ipchchv@gmail.com>
2022-10-27 09:05:40 +0300

    implement a models

---
+++ models.py
@@ -0,0 +1,23 @@
+import datetime
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: str
+    username: str
+    created_at: datetime
+
+
+class Chat(BaseModel):
+    id: str
+    name: str
+    users: [User]
+    created_at: datetime
+
+
+class Message(BaseModel):
+    id: str
+    chat: Chat.id
+    author: User.id
+    text: str
+    created_at: datetime
